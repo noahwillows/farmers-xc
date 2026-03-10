4e1c73021bed4692f72abd43e2b707c24c3d4172
noahwillows <noah.willows@tamu.edu>
2026-03-09 23:07:09 -0500

    Fix duplicate Link import

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Link from "next/link";
-import Link from "next/link";
+
 
 const featureCards = [
   {
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,5 @@
 "use client";
 
-import Link from "next/link";
-import { usePathname } from "next/navigation";
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
